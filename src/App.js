3e38063111fed58a58c2331a13d9c2171bba32f9
Renat <renatilyasov95@gmail.com>
2022-06-09 09:49:51 +0600

    simple tree

--- src/App.js
+++ src/App.js
@@ -1,25 +1,58 @@
-import logo from './logo.svg';
-import './App.css';
+import { useEffect, useState } from "react";
+import {
+  Tree,
+  getBackendOptions,
+  MultiBackend,
+} from "@minoru/react-dnd-treeview";
+import { DndProvider } from "react-dnd";
+import initialData from "./sample-default.json";
+// import initialData from "./d.json";
+import testData from "./view.json";
 
-function App() {
+export default function App() {
+  const [treeData, setTreeData] = useState(initialData);
+  const handleDrop = (newTreeData) => setTreeData(newTreeData);
+
+  // console.log(testData.entityLabelPages[0]);
+
+  const testTree = testData.entityLabelPages[0].labels.map((a, i) => ({
+    id: parseInt(
+      testData.entityLabelPages[0].entityLongIds[i].toString().substring(9),
+      10
+    ),
+    parent:
+      testData.entityLabelPages[0].parentEntityLongIds[i] > 0
+        ? parseInt(
+            testData.entityLabelPages[0].parentEntityLongIds[i]
+              .toString()
+              .substring(9),
+            10
+          )
+        : 0,
+    text: a,
+    droppable: a.includes("element") ? true : false,
+  }));
+  console.log(testTree, "testree");
+  console.log(initialData, "i");
+
+  useEffect(() => {
+    setTreeData(testTree);
+  }, []);
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <DndProvider backend={MultiBackend} options={getBackendOptions()}>
+      <Tree
+        tree={treeData}
+        rootId={0}
+        onDrop={handleDrop}
+        render={(node, { depth, isOpen, onToggle }) => (
+          <div style={{ marginLeft: depth * 10 }}>
+            {node.droppable && (
+              <span onClick={onToggle}>{isOpen ? "[-]" : "[+]"}</span>
+            )}
+            {node.text}
+          </div>
+        )}
+      />
+    </DndProvider>
   );
 }
-
-export default App;
